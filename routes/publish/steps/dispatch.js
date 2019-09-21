3457ed6233f132475cadcd42e873b6d5ebd1199a
Vot <vot@siliconfen.co>
2019-09-21 01:44:02 +0100

    Functional publish process

--- routes/publish/steps/dispatch.js
+++ routes/publish/steps/dispatch.js
@@ -1,8 +1,10 @@
+const _ = require('lodash');
 const debug = require('debug');
 
 module.exports = (req, res) => {
   const stepNumber = Number(req.params.stepNumber);
   debug('octopus:ui:debug')(`Showing Publish step ${stepNumber}`);
+  // debug('octopus:ui:debug')(`Submitted information:\n ${JSON.stringify(req.body, null, 2)}`);
 
   // if not logged in redirect to /users/login with a flash message
 
@@ -12,7 +14,22 @@ module.exports = (req, res) => {
     return res.render('publish/error', res.locals);
   }
 
+  const publicationState = {
+    publicationType: req.body.publicationType,
+    linkedPublications: req.body.linkedPublications,
+    publicationTitle: req.body.publicationTitle,
+    publicationSummary: req.body.publicationSummary,
+    publicationKeywords: req.body.publicationKeywords,
+    collaborators: req.body.collaborators,
+    fundingStatement: req.body.fundingStatement,
+    coiDeclaration: req.body.coiDeclaration,
+    publicationFile: req.body.publicationFile,
+  };
+
   res.locals.publishStepNumber = stepNumber;
-  // debug('octopus:ui:trace')(res.locals);
+  res.locals.publication = publicationState;
+
+  debug('octopus:ui:trace')(res.locals);
+
   return res.render(`publish/steps/step-${stepNumber}`, res.locals);
 };
