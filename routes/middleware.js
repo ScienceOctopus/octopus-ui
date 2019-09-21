cb6e7183fdc7b441e92ad22e88abba342290c580
Vot <vot@siliconfen.co>
2019-09-21 18:51:45 +0100

    Add API authentication

--- routes/middleware.js
+++ routes/middleware.js
@@ -9,6 +9,10 @@ module.exports = (req, res, next) => {
   res.locals.url = req.url;
 
   api.getPublicationTypes((publicationTypesErr, publicationTypes) => {
+    if (publicationTypesErr || !publicationTypes || !publicationTypes.results) {
+      return res.send('Failed to resolve publication types');
+    }
+
     res.locals.publicationTypes = publicationTypes.results;
     return next();
   });
