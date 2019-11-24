5e5f38486870c29e2e43c3d058ae2a2c3c8faae0
Alexandru Patrascu <alexandru.patrascu@wingravity.com>
2019-11-24 18:06:19 +0200

    pdf data

--- routes/publications/downloadPDF.js
+++ routes/publications/downloadPDF.js
@@ -5,22 +5,25 @@ const api = require('../../lib/api');
 
 module.exports = (req, res) => {
   const { publicationID } = req.params;
+  const baseUrl = req.get('host');
 
   debug('octopus:ui:debug')(`Downloading Publication ${publicationID}`);
-
   return api.downloadPublicationPDF(
     publicationID,
+    baseUrl,
     async (publicationErr, publicationPDFBuffer) => {
       if (publicationErr || !publicationPDFBuffer) {
         debug('octopus:ui:error')(
           `Error when trying to download Publication ${publicationID}: ${publicationErr}`
         );
-        return res.render('publications/error');
       }
 
       const publicationPDF = Buffer.from(publicationPDFBuffer);
 
-      res.setHeader('Content-disposition', `attachment; filename=${publicationID}.pdf`);
+      res.setHeader(
+        'Content-disposition',
+        `attachment; filename=${publicationID}.pdf`
+      );
       res.end(publicationPDF, '');
     }
   );
