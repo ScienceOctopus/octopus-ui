3b8e6550b2866ad791671ca6b4b2e808f0f02b0f
Daniel Berigoi <daniel@wingravity.com>
2019-12-13 20:25:14 +0200

    OC-91 - Ability to edit authors

--- routes/publications/edit.js
+++ routes/publications/edit.js
@@ -1,12 +1,17 @@
 const debug = require('debug');
+const _ = require('lodash');
 
 const api = require('../../lib/api');
+const userHelpers = require('../users/helpers');
 
 module.exports = (req, res) => {
   const publicationID = req.params.publicationID;
+  const accessToken = _.get(req, 'session.authOrcid.accessToken');
   debug('octopus:ui:debug')(`Editing Publication ${publicationID}`);
 
-  return api.getPublicationByID(publicationID, (publicationErr, publication) => {
+  return api.getPublicationByID(publicationID, async (publicationErr, publicationData) => {
+    const publication = { ...publicationData };
+
     if (publicationErr || !publication) {
       debug('octopus:ui:error')(`Error when trying to load Publication ${publicationID}: ${publicationErr}`);
       return res.render('publications/error');
@@ -19,6 +24,16 @@ module.exports = (req, res) => {
 
     res.locals.publication = publication;
     res.locals.customTitleTag = `Edit ${publication.title}`;
+
+    if (publication.collaborators) {
+      let authors = publication.collaborators;
+      // Augment authors list
+      authors = await Promise.all(authors.map((author) => userHelpers.findUserByOrcid(author.userID, accessToken)));
+      // Filter our undefined entries
+      authors = authors.filter((author) => author);
+
+      publication.authors = authors;
+    }
 
     // debug('octopus:ui:trace')(res.locals);
     return res.render('publications/edit', res.locals);
