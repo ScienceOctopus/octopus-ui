b67c35da14e6cdff53f25f89b05d22c992f0ae85
Alexandru Patrascu <alexandru.patrascu@wingravity.com>
2020-03-06 10:52:28 +0200

    changes

--- routes/redFlagPublication/addComment.js
+++ routes/redFlagPublication/addComment.js
@@ -4,6 +4,7 @@ const debug = require('debug');
 const api = require('../../lib/api');
 const formHelpers = require('../../lib/form');
 const helpers = require('./helpers');
+const publishHelpers = require('../publish/steps/helpers');
 
 module.exports = async (req, res) => {
   const resolutionID = _.get(req, 'params.resolutionID');
@@ -22,6 +23,8 @@ module.exports = async (req, res) => {
       return res.render('publish/error', { error: err });
     }
 
+    const fileData = _.first(files);
+
     debug('octopus:ui:trace')(fields, files);
     const { text } = fields;
 
@@ -36,6 +39,68 @@ module.exports = async (req, res) => {
     const { comments } = resolution;
 
     comments.push(newComment);
+
+    // return api.getFileContents("5e5f7c84de36713217f1eaf0", (err, data) => {
+    //   if (err) {
+    //     console.log('err', err);
+    //   }
+    //   console.log('data', data);
+    //   function base64ToArrayBuffer(base64) {
+    //     var binaryString = window.atob(base64);
+    //     var binaryLen = binaryString.length;
+    //     var bytes = new Uint8Array(binaryLen);
+    //     for (var i = 0; i < binaryLen; i++) {
+    //        var ascii = binaryString.charCodeAt(i);
+    //        bytes[i] = ascii;
+    //     }
+    //     return bytes;
+    //  }
+
+
+
+    // var byteArray = new Uint8Array(data);
+    // var a = window.document.createElement('a');
+
+    // a.href = window.URL.createObjectURL(new Blob([byteArray], { type: 'application/octet-stream' }));
+    // a.download = data.filename;
+
+    // // Append anchor to body.
+    // document.body.appendChild(a)
+    // a.click();
+
+    // // Remove anchor from body
+    // document.body.removeChild(a)
+
+
+
+  //    function saveByteArray(reportName, byte) {
+  //     var blob = new Blob([byte], {type: "application/pdf"});
+  //     var link = document.createElement('a');
+  //     link.href = window.URL.createObjectURL(blob);
+  //     var fileName = reportName;
+  //     link.download = fileName;
+  //     link.click();
+  // };
+
+  // var sampleArr = base64ToArrayBuffer(data);
+  // saveByteArray("Sample Report", arraybuffer);
+        // return res.redirect(`/resolution-center/${resolutionID}`);
+
+    // })
+
+
+    // if (fileData) {
+    //   console.log('fileData', fileData);
+    //   return publishHelpers.handleFileUpload(fileData, (uploadErr, uploadResult) => {
+    //     if (uploadErr) {
+    //       return res.send('ERROR');
+    //     }
+
+    //     console.log('uploadResult', uploadResult);
+
+    //     return res.redirect(`/resolution-center/${resolutionID}`);
+    //   });
+    // }
 
     // Update Resolution Object
     return api.updateResolution(resolution, (updateErr, updateData) => {
